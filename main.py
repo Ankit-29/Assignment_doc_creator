a1fc49a54057766b9eb7a53f1ee24215606626f4
Ankit-29 <iamankitkumar29@gmail.com>
2020-02-05 02:00:52 +0530

    Changed Sorting function for sorting image names

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import docx
-from docx.shared import Inches
+import re
 import os
+from docx.shared import Inches
 from typing import List
 
 def createDoc(quesfileName: str,allImages: List[str]):
-    file = open(quesfileName,'r')
-    fileList = list(file)
-    # imageIdx = 0
-    questions = list(filter(lambda x: True if(x!='\n') else False,fileList))
-    doc = docx.Document()
-    imageIdx = 0
-    for x in range(len(questions)):
-        quesParaObj = doc.add_paragraph('Question: '+str(x+1)+":")
-        quesParaObj.add_run('\n')
-        quesParaObj.add_run(questions[x])
-        while(imageIdx<len(allImages) and allImages[imageIdx][1]==str(x+1)):
-            print(allImages[imageIdx],imageIdx)
-            doc.add_picture('image/'+allImages[imageIdx],width=Inches(5))
-            imageIdx += 1
+    try:
+        file = open(quesfileName,'r')
+        fileList = list(file)
+        questions = list(filter(lambda x: True if(x!='\n') else False,fileList))
+        doc = docx.Document()
+        imageIdx = 0
+        for x in range(len(questions)):
+            quesParaObj = doc.add_paragraph('Question: '+str(x+1)+":")
+            quesParaObj.add_run('\n')
+            quesParaObj.add_run(questions[x])
+            while(imageIdx<len(allImages) and re.split("[_.]",allImages[imageIdx])[1]==str(x+1)):
+                # print(allImages[imageIdx],imageIdx)
+                doc.add_picture('image/'+allImages[imageIdx],width=Inches(5))
+                imageIdx += 1
 
-    doc.save('assignment.docx')  
+        doc.save('assignment.docx')
+        return True
+    except:
+        return False
+      
 
 def getImagesName() -> List[str]:
+    # Get Image folder path
     imagesPath = os.getcwd()+"/image"
+    
+    # Get all Images names
     allImages = os.listdir(imagesPath)
-    allImages.sort()
+    
+    # Sort Images according to question number
+    allImages.sort(key= lambda x: int(re.split("[_.]",x)[1]))
+
     return allImages
 
-createDoc('questionFile',getImagesName())
+def main():
+    if createDoc('questionFile',getImagesName()):
+        print("Doc Created")
+    else:
+        print("Something Went Wrong")
+    
+main()
