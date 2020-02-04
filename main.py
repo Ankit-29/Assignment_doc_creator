dbb6357dc105326c917a550cbab6950c937a447c
Ankit-29 <iamankitkumar29@gmail.com>
2020-02-05 00:11:00 +0530

    Complete Script With Dummy Files And Images

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import docx
+from docx.shared import Inches
+import os
+from typing import List
 
-def createDoc(fileName: str):
-    file = open(fileName,'r')
+def createDoc(quesfileName: str,allImages: List[str]):
+    file = open(quesfileName,'r')
     fileList = list(file)
+    # imageIdx = 0
     questions = list(filter(lambda x: True if(x!='\n') else False,fileList))
     doc = docx.Document()
+    imageIdx = 0
     for x in range(len(questions)):
-        doc.add_paragraph('Question: '+str(x+1)+":")
-        doc.add_paragraph(questions[x])
-        doc.add_paragraph('')
+        quesParaObj = doc.add_paragraph('Question: '+str(x+1)+":")
+        quesParaObj.add_run('\n')
+        quesParaObj.add_run(questions[x])
+        while(imageIdx<len(allImages) and allImages[imageIdx][1]==str(x+1)):
+            print(allImages[imageIdx],imageIdx)
+            doc.add_picture('image/'+allImages[imageIdx],width=Inches(5))
+            imageIdx += 1
 
     doc.save('assignment.docx')  
 
-createDoc('questionFile')
+def getImagesName() -> List[str]:
+    imagesPath = os.getcwd()+"/image"
+    allImages = os.listdir(imagesPath)
+    allImages.sort()
+    return allImages
+
+createDoc('questionFile',getImagesName())
